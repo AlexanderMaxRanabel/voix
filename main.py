d91bd14566bc0b45a07027a7fffd229426e55422
choroalp <choroalp@skiff.com>
2024-07-04 16:52:08 +0300

    Added notes

--- main.py
+++ main.py
@@ -39,6 +39,27 @@ def make_cosine(frequency, duration, sample_rate, amplitude, phase):
 
     return audio
 
+def mk_note(note, cosine_or_sine, duration, sample_rate):
+    audio = None
+
+    note_frequencies = {
+        'C4': 261.63,
+        'D4': 293.66,
+        'E4': 329.63,
+        'F4': 349.23,
+        'G4': 392.00,
+        'A4': 440.00,
+        'B4': 493.88,
+        'C5': 523.25
+    }
+
+    match cosine_or_sine:
+        case 'cosine':
+            audio = make_cosine(note_frequencies[note], duration, sample_rate, 0.5, 0)
+        case 'sine':
+            audio = make_sine(note_frequencies[note], duration, sample_rate, 0.5)
+        case _:
+            print("Unknown Wave Type")
 
 def runtime(filename):
     playable_flow = []
@@ -66,6 +87,13 @@ def runtime(filename):
                     audio = make_cosine(frequency, duration, sample_rate, amplitude, phase)
                     playable_flow.append(audio)
                     print(playable_flow)
+                case 'auto_note':
+                    note = float(tokens[1])
+                    duration = float(tokens[2])
+                    cosine_or_sine = float(tokens[4])
+                    sample_rate = float(tokens[5])
+                    audio = mk_note(note, duration, cosine_or_sine, sample_rate)
+
                 case 'play_flow':
                     play_flow(playable_flow, sample_rate)
                 case _: print("Unknown Keyword")
